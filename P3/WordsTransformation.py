67e280abb37f9a8cae32997d187f79b747cd5578
Yulia5 <yvoev@mail.ru>
2015-07-05 20:35:51 +0100

    wordsTransformation: optimisation, validation, test cases
    wordsTransformation: optimisation, validation, test cases

--- P3/WordsTransformation.py
+++ P3/WordsTransformation.py
@@ -1,12 +1,11 @@
 import enchant
 from functools import total_ordering
 from string import ascii_lowercase
-from operator import pos
-dictEng = enchant.Dict("en_US")
-ascii_lowercase_reversed = [ascii_lowercase[i] for i in range(len(ascii_lowercase)-1, -1, -1)]
 
 @total_ordering
 class WordWithPath:
+    dictEng = enchant.Dict("en_US")
+    
     def __init__(self, newWord, ancestor = None):
         self.word = newWord
         if ancestor == None:
@@ -19,34 +18,41 @@ class WordWithPath:
         return (self.word == other.word)
     def __lt__(self, other):
         return (self.word < other.word)
-    def reversePath(self):
-        return [self.path[i] for i in range(len(self.path)-1, -1, -1)]                        
-
-#find all words that are 1 letter different            
-def findNextWords(wordWithPath):
-    w = wordWithPath.word
-    result = []
-    # all words before w
-    for pos in range(len(w)):
-        originalLetter = w[pos]
-        for c in ascii_lowercase:
-            w = w[:pos] + c + w[pos+1:] 
-            if c == originalLetter:
-                break
-            if dictEng.check(w):
-                result.append(WordWithPath(w, wordWithPath))
     
-    for pos in range(len(w)-1, -1, -1):
-        originalLetter = w[pos]
-        for c in ascii_lowercase:
-            if c <= originalLetter:
-                continue
-            w = w[:pos] + c + w[pos+1:] 
-            if dictEng.check(w):
-                result.append(WordWithPath(w, wordWithPath))
-        w = w[:pos] + originalLetter + w[pos+1:]
-   
-    return result         
+    #for the result outputting
+    def concatenatePath(self, rhs):
+        result = [self.path[i] for i in range(len(self.path)-1, -1, -1)] + [self.word] + rhs.path
+        return result
+    
+    @staticmethod
+    def checkWord(w):
+        return WordWithPath.dictEng.check(w)
+    
+    #find all words that are 1 letter different            
+    def findNextWords(self):
+        w = self.word #a shortcut
+        result = []
+        # all words before w
+        for pos in range(len(w)):
+            originalLetter = w[pos]
+            for c in ascii_lowercase:
+                w = w[:pos] + c + w[pos+1:] 
+                if c == originalLetter:
+                    break
+                if WordWithPath.dictEng.check(w):
+                    result.append(WordWithPath(w, self))
+        
+        for pos in range(len(w)-1, -1, -1):
+            originalLetter = w[pos]
+            for c in ascii_lowercase:
+                if c <= originalLetter:
+                    continue
+                w = w[:pos] + c + w[pos+1:] 
+                if WordWithPath.dictEng.check(w):
+                    result.append(WordWithPath(w, self))
+            w = w[:pos] + originalLetter + w[pos+1:]
+       
+        return result         
         
 
 def mergeSortedLists(list1, list2):
@@ -56,18 +62,21 @@ def mergeSortedLists(list1, list2):
         if (i1 < len(list1)) and (i2 < len(list2)):
             if (list1[i1] < list2[i2]):
                 result.append(list1[i1]); i1 = i1+1
-            elif (list1[i1] > list2[i2]): 
-                result.append(list2[i2]); i2 = i2+1
-            else:
-                result.append(list2[i2]); i1 = i1+1; i2 = i2+1
+            else: 
+                result.append(list2[i2])
+                if (list1[i1] == list2[i2]): # avoid duplicates
+                    i1 = i1+1
+                i2 = i2+1
+            continue
         elif (i1 < len(list1)):
-            result.append(list1[i1]); i1 = i1+1
+            result += list1[i1:]
         elif (i2 < len(list2)):
-            result.append(list2[i2]); i2 = i2+1
-        else:
-            return result
+            result += list2[i2:]
+        return result
         
 def mergeManySortedLists(allLists):
+    if len(allLists) == 0:
+        return []
     if len(allLists) == 1:
         return allLists[0]
     i = 0
@@ -78,7 +87,7 @@ def mergeManySortedLists(allLists):
         else:
             newAllLists.append(allLists[2*i])
         i = i+1
-    allLists = [] # clear memory
+    allLists = []# clear memory
     return mergeManySortedLists(allLists = newAllLists)
 
 def findOverlaps(list1, list2):
@@ -105,53 +114,53 @@ def removeOverlaps(listToClean, listReference):
         else:
             i2 = i2+1    
 
-def searchInSortedList(sortedList, searchee):
-    lowerBound = 0
-    upperBound = len(sortedList)-1
-    while (lowerBound < upperBound):
-        mid = int(round((lowerBound+upperBound)/2))
-        if (sortedList[mid] == searchee):
-            return mid
-        if (sortedList[mid] < searchee):
-            lowerBound = mid+1
-        else:
-            upperBound = mid-1 
-        if (lowerBound == upperBound):
-            if (sortedList[lowerBound] == searchee):
-                return lowerBound        
-    return None
-
-def findChain(leftWord, rightWord):
-    leftSet = [WordWithPath(leftWord)]; leftEdge = [WordWithPath(leftWord)]
-    rightSet = [WordWithPath(rightWord)]; rightEdge = [WordWithPath(rightWord)]     
-    for j in range(10):
-        newLeftEdge = mergeManySortedLists([findNextWords(w) for w in leftEdge])
-        leftSet = mergeSortedLists(leftEdge, leftSet)
-        removeOverlaps(listToClean = newLeftEdge, listReference = leftSet)
-        leftEdge = newLeftEdge
-        
-        print [n.word for n in newLeftEdge]
-        
-        overlaps = findOverlaps(leftEdge, rightEdge)
+def findChain(leftWord, rightWord, maxTreeDepth):
+    errors = ''
+    if len(leftWord) != len(rightWord):
+        errors = ("Lengths of the left word, '" + leftWord 
+                         + "', and the right word, '" + rightWord + "' do not match. ")
+    if not WordWithPath.checkWord(leftWord):
+        errors += "Left word, '" + leftWord + "', is not in the dictionary. "
+    if not WordWithPath.checkWord(rightWord):
+        errors += "Right word, '" + rightWord + "', is not in the dictionary. "
+    if errors != '':
+        raise ValueError(errors)
+    if (leftWord == rightWord):
+        return [WordWithPath(leftWord), WordWithPath(rightWord)]
+    
+    leftAllNodes = [WordWithPath(leftWord)]; leftLeaves = [WordWithPath(leftWord)]
+    rightAllNodes = [WordWithPath(rightWord)]; rightLeaves = [WordWithPath(rightWord)]     
+    for _ in range(maxTreeDepth):
+        newLeftLeaves = mergeManySortedLists([w.findNextWords() for w in leftLeaves])
+        leftAllNodes = mergeSortedLists(leftLeaves, leftAllNodes)
+        removeOverlaps(listToClean = newLeftLeaves, listReference = leftAllNodes)
+        leftLeaves = newLeftLeaves        
+        overlaps = findOverlaps(leftLeaves, rightLeaves)
         if len(overlaps)>0:
-            return overlaps
-        newRightEdge = mergeManySortedLists([findNextWords(w) for w in rightEdge])
-        rightSet = mergeSortedLists(rightEdge, rightSet)
-        removeOverlaps(listToClean = newRightEdge, listReference = rightSet)
-        rightEdge = newRightEdge
+            break
         
-        print [n.word for n in newRightEdge]
-         
-        overlaps = findOverlaps(leftEdge, rightEdge)
+        newRightLeaves = mergeManySortedLists([w.findNextWords() for w in rightLeaves])
+        rightAllNodes = mergeSortedLists(rightLeaves, rightAllNodes)
+        removeOverlaps(listToClean = newRightLeaves, listReference = rightAllNodes)
+        rightLeaves = newRightLeaves        
+        overlaps = findOverlaps(leftLeaves, rightLeaves)
         if len(overlaps)>0:
-            return overlaps      
+            break      
        
-    return []
+    return [h1.concatenatePath(h2) for h1, h2 in overlaps] 
 
 
 if __name__ == '__main__':
-    halfChains = findChain("man", "spa")
-    for h1, h2 in halfChains:
-        c = h1.reversePath() + [h1.word] + h2.path
-        print c
+    allPaths = findChain("man", "spa", 10)
+    for p in allPaths:
+        print p
+    allPaths = findChain("tree", "fled", 10)
+    for p in allPaths:
+        print p
+    allPaths = findChain("ree", "fled", 10)
+    for p in allPaths:
+        print p
+    allPaths = findChain("zree", "fled", 10)
+    for p in allPaths:
+        print p
     
