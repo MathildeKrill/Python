f32a4394abf3734cc5777c31a942908a0901353f
Yulia5 <yvoev@mail.ru>
2015-07-10 17:56:50 +0100

    BinaryTreeMusings: added generic graphs; bug fix in trees to account for recombining trees
    BinaryTreeMusings: added generic graphs and their basic traversals; bug
    fix in trees to account for recombining trees

--- P3/BinaryTreeMusings.py
+++ P3/BinaryTreeMusings.py
@@ -9,13 +9,45 @@ def shuffle(upper_limit):
         next_elem = random.randint(0, len(unshuffled_numbers) - 1)
         result.append(unshuffled_numbers[next_elem])
         del unshuffled_numbers[next_elem]
-    return result           
+    return result 
+
+class Graph:
+    def __init__(self, number_nodes, max_number_edges = 0.5): 
+        if (max_number_edges > 1) or (max_number_edges < 0):
+            raise ValueError("max_number_edges is out of range")
+        self.nodes_connections = {node_nb : range(node_nb+1, number_nodes) for node_nb in xrange(number_nodes)}
+        for key, value in self.nodes_connections.iteritems():
+            while len(value) > (max_number_edges * (number_nodes - key - 1)):
+                index_to_remove = random.randint(0, len(value)-1)
+                del value[index_to_remove]
+                                
+            new_scheduled_nodes_ids += [key for key, value in self.nodes_connections.iteritems() 
+                                            if (node_id in value) and not (key in visited_nodes_ids or key in scheduled_nodes_ids)] 
+            scheduled_nodes_ids += new_scheduled_nodes_ids 
+            visited_nodes_ids.append(node_id)
+            del scheduled_nodes_ids[0]
+        return visited_nodes_ids
+    
+    def __IterateInDepth(self, start_node_id, visited_nodes):
+        visited_nodes.append(start_node_id)
+        for key, value in self.nodes_connections.iteritems():
+            if start_node_id in value:
+                if key not in visited_nodes:
+                    self.__IterateInDepth(start_node_id = key, visited_nodes = visited_nodes)
+        for value in self.nodes_connections[start_node_id]:
+            if value not in visited_nodes:
+                self.__IterateInDepth(start_node_id = value, visited_nodes = visited_nodes)            
+        
+    def IterateInDepth(self, start_node_id):
+        visited_nodes = []
+        self.__IterateInDepth(start_node_id, visited_nodes = visited_nodes)
+        return visited_nodes
      
 class BinaryTreeNode:
     def __init__(self, node_id, left_child = None, right_child = None):
         self.node_id = node_id
         self.left_child = left_child
-        self.right_child = right_child
+        self.right_child = right_child 
     
     def print_node(self):
         result = str(self.node_id) + ", "
@@ -93,16 +125,24 @@ class BinaryTree:
     def IterateInBreadth(self):
         if not self.all_nodes:
             return []
-        visited_nodes = []
+        visited_nodes_ids = []
         scheduled_nodes = [self.all_nodes[0]]
+        scheduled_nodes_ids = [self.all_nodes[0].node_id]
         while scheduled_nodes:
             if scheduled_nodes[0].left_child:
-                scheduled_nodes.append(scheduled_nodes[0].left_child)
+                node_id = scheduled_nodes[0].left_child
+                if not (node_id in scheduled_nodes_ids) and not (node_id in visited_nodes_ids):
+                    scheduled_nodes.append(scheduled_nodes[0].left_child)
+                    scheduled_nodes_ids.append(node_id)
             if scheduled_nodes[0].right_child:
-                scheduled_nodes.append(scheduled_nodes[0].right_child)
-            visited_nodes.append(scheduled_nodes[0].node_id)
+                node_id = scheduled_nodes[0].right_child
+                if not (node_id in scheduled_nodes_ids) and not (node_id in visited_nodes_ids):
+                    scheduled_nodes.append(scheduled_nodes[0].right_child)
+                    scheduled_nodes_ids.append(node_id)
+            visited_nodes_ids.append(scheduled_nodes[0].node_id)
             del scheduled_nodes[0]
-        return visited_nodes
+            del scheduled_nodes_ids[0]
+        return visited_nodes_ids
 
     def __IterateInDepth(self, root):
         if not root:
@@ -141,9 +181,16 @@ class BinaryTree:
             return right_ancestor
 
 if __name__ == '__main__':
-    #random.seed(10000)
+    #random.seed(1000)
     tree = BinaryTree(nodes_qty = 15, shuffle_node_ids = False, tree_type = "balanced")#"rightChildIsChildless")#
     tree.Print()
     x = tree.IterateInBreadth(); print x # x.sort(); 
     x = tree.IterateInDepth(); print x # x.sort(); 
     print tree.DeepestCommonAncestor(8, 14).node_id
+    
+    g = Graph(number_nodes = 11, max_number_edges = 0.33)
+    g.Print()
+    print g.IterateInBreadth(8)
+    print g.IterateInBreadth(0)
+    print g.IterateInDepth(8)
+    print g.IterateInDepth(0)
