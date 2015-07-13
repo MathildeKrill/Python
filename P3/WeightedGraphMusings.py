fe0f007802e7a4fde6d36518a6d2b13bf0786575
Yulia5 <yvoev@mail.ru>
2015-07-13 17:17:08 +0100

    Added weighted graphs with Bellman-Ford algorithm
    Added weighted graphs with Bellman-Ford algorithm

---
+++ P3/WeightedGraphMusings.py
@@ -0,0 +1,78 @@
+import random
+
+class WeightedGraph:
+    def __init__(self, number_vertice, 
+                 min_edges_per_vertice, 
+                 max_edges_per_vertice, 
+                 negative_weights_allowed = False):
+        assert number_vertice > 0
+        assert min_edges_per_vertice >= 0
+        assert max_edges_per_vertice < number_vertice 
+        assert min_edges_per_vertice <= max_edges_per_vertice 
+        if negative_weights_allowed:
+            self.edges_per_vertice = [[(v, 2*random.random()-1) for v in range(number_vertice)] for _ in range(number_vertice)]
+        else:
+            self.edges_per_vertice = [[(v, random.random()) for v in range(number_vertice)] for _ in range(number_vertice)]
+        counter = -1
+        for edge_per_vertice in self.edges_per_vertice:
+            counter += 1
+            # to make sure there are no self loops, replace it by the end of the list 
+            edge_per_vertice[counter] = edge_per_vertice[-1] 
+            number_of_edges = random.randint(min_edges_per_vertice, max_edges_per_vertice)
+            for nb_e in range(number_of_edges):
+                buffer_ = edge_per_vertice[nb_e]
+                swap_element_nb = random.randint(nb_e, number_vertice-2)
+                edge_per_vertice[nb_e] = edge_per_vertice[swap_element_nb]
+                edge_per_vertice[swap_element_nb] =  buffer_
+            del  edge_per_vertice[number_of_edges:]
+            edge_per_vertice.sort(key = lambda e: e[0])
+            
+    def Print(self):
+        vertex = -1
+        for epv in self.edges_per_vertice: # iterate over ends of the edge
+            vertex += 1
+            for other_vertice, weight in epv:
+                print "edge (", vertex, ", ", other_vertice, "), weight = ", weight
+             
+    def BellmanFordPath(self, single_source):
+        assert single_source >= 0
+        assert single_source < len(self.edges_per_vertice)
+        
+        result = [(None, None) for _ in self.edges_per_vertice]
+        result[single_source] = (0, None) # distance to itself is 0
+        negative_loop_edge = None
+        for iteration in range(len(self.edges_per_vertice)): # iterate over lengths of paths, last loop for checking
+            for vertex_from in range(len(self.edges_per_vertice)): # iterate over beginning of the edge
+                for vertex_to, weight in self.edges_per_vertice[vertex_from]:
+                    if result[vertex_to][0] == None: # we differentiate between 0 and None
+                        continue # we cannot relax the edge that comes from unreachable vertice
+                    new_distance = result[vertex_to][0] + weight
+                    if (result[vertex_from][0] == None) or (result[vertex_from][0] > new_distance):
+                        if iteration == len(self.edges_per_vertice) - 1:
+                            negative_loop_edge = (vertex_from, vertex_to)
+                        else:
+                            result[vertex_from] = (new_distance, vertex_to)
+                                                    
+        for vertex in xrange(len(result)):
+            print "vertex ", vertex, ", distance ", result[vertex][0], ", ancestor ", result[vertex][1]
+            
+        if negative_loop_edge:
+            print "negative_loop_edge: ", negative_loop_edge[0], ", ", negative_loop_edge[1]
+            
+        return result, negative_loop_edge
+                    
+
+if __name__ == '__main__':
+    random.seed(10000)
+    wg = WeightedGraph(5, 0, 0)
+    wg.Print()
+    wg.BellmanFordPath(single_source = 0)
+    wg = WeightedGraph(5, 4, 4)
+    wg.Print()
+    wg.BellmanFordPath(single_source = 0)
+    wg = WeightedGraph(5, 2, 3)
+    wg.Print()
+    wg.BellmanFordPath(single_source = 0)
+    wg = WeightedGraph(5, 1, 1, True)
+    wg.Print()
+    wg.BellmanFordPath(single_source = 0)
