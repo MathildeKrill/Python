b58c6e5a2a2a7d9711adb8b19b2e10d20676020d
Yulia V <yuliavoevodskaya@Yulias-MacBook-Pro.local>
2017-07-30 18:16:59 +0100

    printing and error checks clean up

--- P3/test/shift_digit.py
+++ P3/test/shift_digit.py
@@ -13,14 +13,17 @@ import decimal
 MAX_POWER = 100
 MAX_ERROR = 1E-16
 
-def _print_float(name_of_f, f):
-    return name_of_f + ' = ' + ('{:' +str(MAX_POWER + 2) + '.8f}').format(f)
+def _is_not_0(error):
+    return (error > MAX_ERROR) or (error < (-MAX_ERROR))
 
 def _print_int(name_of_i, i):
     return name_of_i + ' = ' + ('{:' +str(MAX_POWER + 2) + '.0f}').format(i)
 
 def find_a_b(base, factor, pow):
     _a = Decimal(Decimal(pow - factor)/Decimal(base * factor - Decimal(1)))
+    a_error = _a - _a.quantize(Decimal(1.0)); # non-integer part
+    if _is_not_0(error = a_error):
+        print 'ERROR WITH ' + _print_int('pow', pow) + _print_int('factor', factor) + _print_int('base', base)
     a = Decimal(0)
     b = Decimal(0)
     for _ in range(1, base):
@@ -30,9 +33,9 @@ def find_a_b(base, factor, pow):
         ba = Decimal(a + pow * b)
         error = Decimal(ba - factor * ab)
         if a >= pow : # should never happen
-            print 'ERROR overflow WITH ' + _print_float('a', a) + _print_int('b', b) + _print_int('pow', pow)
-        if error * error > (MAX_ERROR * MAX_ERROR):  # should never happen
-            print 'ERROR WITH ' + _print_float('a', a) + _print_int('b', b) + _print_float('ba - factor * ab', error)
+            print 'ERROR overflow WITH ' + _print_int('a', a) + _print_int('b', b) + _print_int('pow', pow)
+        if _is_not_0(error):  # should never happen
+            print 'ERROR WITH ' + _print_int('a', a) + _print_int('b', b) + _print_int('ba - factor * ab', error)
 #         print _print_float('a', a) 
 #         for k, v in {'b' : b, 'ab' : ab, 'ba' : ba}.items():
 #             print  _print_int(k, v)
