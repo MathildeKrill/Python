1554df4779f2aa1865a545d70f94c4e56b2d63ea
Yulia V <yvoev@mail.ru>
2018-03-31 19:21:53 +0100

    Works but music quality drops and subtitles not pretty

--- P3/test/youtube.py
+++ P3/test/youtube.py
@@ -3,21 +3,29 @@ Created on 30 Mar 2018
 
 @author: yuliavoevodskaya
 '''
-import cv2, os, pydub, os
+import cv2, os, pydub, os, shlex, codecs, shutil, copy
 from pydub import AudioSegment
 
+def get_temp_folder(directory_name):
+    return os.path.join(directory_name, 'temp2')
+
 def make_temp_dir(directory_name):
-    temp_folder = os.path.join(directory_name, 'temp2')
-    if not os.path.exists(temp_folder):
-        os.makedirs(temp_folder)
+    temp_folder = get_temp_folder(directory_name)
+    if os.path.exists(temp_folder):
+        shutil.rmtree(temp_folder)
+    os.makedirs(temp_folder)
     return temp_folder
 
-def make_long_mp3(directory_name, width, height):
+# do something on all lines of a file
+my_encoding = 'utf-8'
+
+def write_to_file(file_path, file_content):
+    with codecs.open(file_path, 'w', encoding = my_encoding) as the_file:
+        the_file.writelines(file_content)
+
+def make_short_videos(directory_name, width, height):
     
     temp_folder = make_temp_dir(directory_name)
-    output_name = os.path.basename(os.path.dirname(directory_name)) 
-    output_video_name = os.path.join(temp_folder, output_name + '.mp4') 
-    output_audio_name = os.path.join(temp_folder, output_name + '.mp3')
 
     images_filenames =  [os.path.join(directory_name, fn) for fn in os.listdir(directory_name) 
                             if (fn.endswith('.jpg') or (fn.endswith('.png')))]
@@ -26,21 +34,21 @@ def make_long_mp3(directory_name, width, height):
     images = [cv2.resize(img, (width, height)) for img in images]       
         
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        
-    one_sec_pause = AudioSegment.silent(duration=1000) # 1 sec    
-    
     audio_filenames =  [os.path.join(directory_name, fn) for fn in os.listdir(directory_name) 
                             if fn.endswith('.mp3')]
     audio_filenames.sort()
-    counter_images = 0
+    counter_audio = 0
     for audio_name in audio_filenames:
         print(audio_name)    
         audio_piece = AudioSegment.from_mp3(audio_name)
         audio_mediainfo = pydub.utils.mediainfo(audio_name).get('TAG', None)
-        title = audio_mediainfo['title'] + '\n' + audio_mediainfo['TCM']
-        print(title)
         
         print(str(audio_piece.duration_seconds))
+        img = copy.deepcopy(images[counter_audio % len(images)])
+        counter_audio = counter_audio + 1
+        track_name = 'Track ' + str(counter_audio) + ": " + audio_mediainfo['title']
+        cv2.putText(img, track_name            , (10,600), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), cv2.LINE_4)       
+        cv2.putText(img, audio_mediainfo['TCM'], (10,700), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), cv2.LINE_4)       
         
         # create a silent video
         silent_video_name = os.path.join(temp_folder, os.path.basename(audio_name)[:-4] + '_silent.mp4')
@@ -48,22 +56,35 @@ def make_long_mp3(directory_name, width, height):
         video = cv2.VideoWriter(silent_video_name, fourcc, 0.5, (width, height))
         counter_frames = 0
         while (counter_frames < (audio_piece.duration_seconds + 3)):
-            img = images[counter_images]
-            #cv2.addText(img, title, (10,500), cv2.FONT_HERSHEY_SIMPLEX)       
             video.write(img) 
             counter_frames = counter_frames + 2       
         cv2.destroyAllWindows()
         video.release()
-        counter_images = (counter_images + 1) % len(images)
         
         # add audio
-        video_name = os.path.join(temp_folder, os.path.basename(audio_name)[:-4] + '.mp4')
+        video_name = os.path.join(temp_folder, os.path.basename(audio_name)[:-4].replace(',', '').replace("'", '').replace(" ", '') + '.mp4')
         os.system('ffmpeg -i "' + silent_video_name + '" -i "' + audio_name + '" -shortest -c:v copy -c:a aac -b:a 256k "' + video_name + '"')
-        
-    #audio.export(compilation_audio_name, format="mp3")    
+        os.remove(silent_video_name)
     
+def join_videos(directory_name):
+    temp_folder = get_temp_folder(directory_name)
+    output_name = os.path.basename(os.path.dirname(directory_name)) 
+    output_video_name = os.path.join(temp_folder, output_name + '.mp4') 
+    output_text_name = os.path.join(temp_folder, output_name + '.txt')
+    if os.path.exists(output_video_name):
+        os.remove(output_video_name)   
+
+    video_filenames =  [os.path.join(temp_folder, fn) for fn in os.listdir(temp_folder) 
+                            if fn.endswith('.mp4')]
+    video_filenames.sort()
+    video_filenames = ['file ' + shlex.quote(video_name) for video_name in video_filenames]     
+    print(video_filenames)
+    write_to_file(file_path = output_text_name, file_content = '\n'.join(video_filenames))
+    
+    os.system('ffmpeg -f concat -safe 0 -i "' + output_text_name + '" -c copy "' + output_video_name + '"')     
 
 if __name__ == '__main__':
     dir_path = os.path.expanduser('~/Music/LouisXIII/')
-    make_long_mp3(dir_path, 1280, 720)
+    make_short_videos(dir_path, 1280, 720)
+    join_videos(dir_path)
     print("done")
