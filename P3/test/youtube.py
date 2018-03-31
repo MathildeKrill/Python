97372fd73117595e755e61531a526de7c981f12d
Yulia V <yvoev@mail.ru>
2018-03-31 20:53:37 +0100

    combine videos and audios separately

--- P3/test/youtube.py
+++ P3/test/youtube.py
@@ -3,7 +3,7 @@ Created on 30 Mar 2018
 
 @author: yuliavoevodskaya
 '''
-import cv2, os, pydub, os, shlex, codecs, shutil, copy
+import cv2, os, pydub, os, codecs, shutil, copy
 from pydub import AudioSegment
 
 def get_temp_folder(directory_name):
@@ -38,55 +38,52 @@ def make_short_videos(directory_name, width, height):
                             if fn.endswith('.mp3')]
     audio_filenames.sort()
     counter_audio = 0
+    counter_seconds = 0
+    
+    # create a silent video
+    silent_video_name = os.path.join(temp_folder, os.path.basename(directory_name) + '_silent.mp4')
+    video = cv2.VideoWriter(silent_video_name, fourcc, 1.0, (width, height))
+    
+    #combine mp3s
+    audio = AudioSegment.silent(duration = 0)
+    
     for audio_name in audio_filenames:
-        print(audio_name)    
+        # print(audio_name)    
         audio_piece = AudioSegment.from_mp3(audio_name)
-        audio_mediainfo = pydub.utils.mediainfo(audio_name).get('TAG', None)
-        
-        print(str(audio_piece.duration_seconds))
+        audio_mediainfo = pydub.utils.mediainfo(audio_name).get('TAG', None)       
+        # print(str(audio_piece.duration_seconds))
         img = copy.deepcopy(images[counter_audio % len(images)])
         counter_audio = counter_audio + 1
         track_name = 'Track ' + str(counter_audio) + ": " + audio_mediainfo['title']
         cv2.putText(img, track_name            , (10,600), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), cv2.LINE_4)       
         cv2.putText(img, audio_mediainfo['TCM'], (10,700), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), cv2.LINE_4)       
-        
-        # create a silent video
-        silent_video_name = os.path.join(temp_folder, os.path.basename(audio_name)[:-4] + '_silent.mp4')
-        print(silent_video_name)
-        video = cv2.VideoWriter(silent_video_name, fourcc, 0.5, (width, height))
+      
         counter_frames = 0
-        while (counter_frames < (audio_piece.duration_seconds + 3)):
+        while (counter_frames < (audio_piece.duration_seconds + 1)):
             video.write(img) 
-            counter_frames = counter_frames + 2       
-        cv2.destroyAllWindows()
-        video.release()
-        
-        # add audio
-        video_name = os.path.join(temp_folder, os.path.basename(audio_name)[:-4].replace(',', '').replace("'", '').replace(" ", '') + '.mp4')
-        os.system('ffmpeg -i "' + silent_video_name + '" -i "' + audio_name + '" -shortest -c:v copy -c:a aac -b:a 256k "' + video_name + '"')
-        os.remove(silent_video_name)
+            counter_frames = counter_frames + 1
+            counter_seconds = counter_seconds + 1
+            
+        audio = audio + audio_piece
+        # match the duration of audio and video so far
+        audio = audio + AudioSegment.silent(duration =  (counter_seconds * 1000.0 - len(audio))) 
+                  
+    cv2.destroyAllWindows()
+    video.release()
+    print('1')
+    # dump the long mp3
+    compilation_audio_name = os.path.join(temp_folder, os.path.basename(directory_name) + '.mp3')
+    audio.export(compilation_audio_name, format = "mp3")   
+    print('2')    
+    # add audio and remove the intermediate files
+    video_name = os.path.join(directory_name, os.path.basename(directory_name) + '.mp4')
+    os.system('ffmpeg -i "' + silent_video_name + '" -i "' + compilation_audio_name + '" -shortest -c:v copy -c:a aac -b:a 256k "' + video_name + '"')
+    os.remove(silent_video_name)
+    os.remove(compilation_audio_name)
     
-def join_videos(directory_name):
-    temp_folder = get_temp_folder(directory_name)
-    output_name = os.path.basename(os.path.dirname(directory_name)) 
-    output_video_name = os.path.join(directory_name, output_name + '.mp4') 
-    output_text_name = os.path.join(directory_name, output_name + '.txt')
-    if os.path.exists(output_video_name):
-        os.remove(output_video_name)   
-
-    video_filenames =  [os.path.join(temp_folder, fn) for fn in os.listdir(temp_folder) 
-                            if fn.endswith('.mp4')]
-    video_filenames.sort()
-    video_filenames = ['file ' + shlex.quote(video_name) for video_name in video_filenames]     
-    print(video_filenames)
-    write_to_file(file_path = output_text_name, file_content = '\n'.join(video_filenames))
-    
-    ffmpeg_line = 'ffmpeg -f concat -safe 0 -i "' + output_text_name + '" -c copy "' + output_video_name + '"'
-    print(ffmpeg_line)
-    os.system(ffmpeg_line)     
 
 if __name__ == '__main__':
-    dir_path = os.path.expanduser('~/Music/LouisXIII copy/')
+    dir_path = os.path.expanduser('~/Music/LouisXIII')
     make_short_videos(dir_path, 1280, 720)
-    join_videos(dir_path)
+    #join_videos(dir_path)
     print("done")
