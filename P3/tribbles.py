0feefe14dc496c3df48486b222e62e4a5c2a0fce
Yulia V <yvoev@mail.ru>
2019-03-20 05:56:20 +0000

    cleanup

--- P3/tribbles.py
+++ P3/tribbles.py
@@ -27,11 +27,11 @@ def get_tribbles_survival_probability_growth(nb_paths, prob_choose_mountains, pr
     total_population_growth_log_sqr = 0.0
     
     for _ in range(nb_paths):
-        population = tribbles_one_path(prob_choose_mountains = prob_choose_mountains, 
-                                       prob_rain = prob_rain, 
-                                       nb_children = nb_children, 
-                                       init_population = init_population, 
-                                       nb_generations = nb_generations)
+        population = tribbles_one_path(prob_choose_mountains=prob_choose_mountains, 
+                                       prob_rain=prob_rain, 
+                                       nb_children=nb_children, 
+                                       init_population=init_population, 
+                                       nb_generations=nb_generations)
         if population < 1.0:
             nb_extint += 1.
         else:
@@ -45,39 +45,35 @@ def get_tribbles_survival_probability_growth(nb_paths, prob_choose_mountains, pr
             total_population_growth_log / nb_paths, 
             math.sqrt(max(variance, 0.0) / nb_paths), variance ]
  
-def write_to_file(file_path, file_content, my_encoding = 'utf-8'):
-    with codecs.open(file_path, 'w', encoding = my_encoding) as the_file:
+def write_to_file(file_path, file_content, my_encoding='utf-8'):
+    with codecs.open(file_path, 'w', encoding=my_encoding) as the_file:
         the_file.writelines(file_content)
-           
-def append_to_file(file_path, new_lines, my_encoding = 'utf-8'):
-    with codecs.open(file_path, encoding = my_encoding) as file_opened:
-        file_content_list = file_opened.readlines()
-    write_to_file(file_path, file_content_list + new_lines, my_encoding = my_encoding)
     
-def array_to_line(an_array, separator = '|'):
-    result = '|'.join([str(x) for x in an_array]) + '\n'
+def array_to_line(an_array, separator='|'):
+    result = separator.join([str(x) for x in an_array]) + '\n'
     return result
 
 if __name__ == '__main__':
     file_path = os.path.expanduser('~/Documents/Sites/wordpress-yu51a5/tribbles.txt')
-    title_line = ['prob choose mountains', 'prob rain', 'nb children',
+    title_line = ['prob rain', 'nb children', 'prob to choose mountains', 
                                            'prob population survival', 'growth rate', 'growth rate std dev', 'growth rate var']
     file_contents = [array_to_line(title_line)]
-    for percentage_choose_mountains in range(0, 101):
-        for percentage_rains in range(0, 101):
-            for nb_children in (2, 3, 5, 8):
-                new_line = [percentage_choose_mountains / 100.0, percentage_rains / 100.0, nb_children]
+    for percentage_rains in range(0, 101):
+        for nb_children in (2, 3, 5, 8):
+            for percentage_choose_mountains in range(0, 101):
+                new_line = [percentage_rains / 100.0, nb_children, percentage_choose_mountains / 100.0]
                 try:
                     survival_prob_growth = get_tribbles_survival_probability_growth(
-                                                      nb_paths = 10000, 
-                                                      prob_choose_mountains = percentage_choose_mountains / 100.0, 
-                                                      prob_rain = percentage_rains / 100.0, 
-                                                      nb_children = nb_children, 
-                                                      init_population = 4000, 
-                                                      nb_generations = 300)
+                                                      nb_paths=10000, 
+                                                      prob_choose_mountains=percentage_choose_mountains / 100.0, 
+                                                      prob_rain=percentage_rains / 100.0, 
+                                                      nb_children=nb_children, 
+                                                      init_population=4000, 
+                                                      nb_generations=300)
                     new_line += survival_prob_growth
                 except:
                     print(new_line)
                 file_contents.append(array_to_line(new_line))
                 
-    write_to_file(file_path = file_path, file_content = file_contents)
+    write_to_file(file_path=file_path, file_content=file_contents)
+    
