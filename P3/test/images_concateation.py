a5e23d7ddb77c27b11dab4dce89ea9c393b4258d
Yulia V <yvoev@mail.ru>
2019-08-14 13:29:49 +0300

    improved images concatenation

--- P3/test/images_concateation.py
+++ P3/test/images_concateation.py
@@ -1,5 +1,4 @@
-import numpy as np
-import cv2, os
+import numpy, cv2, os
 import urllib.request
 
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
@@ -10,7 +9,7 @@ def url_to_image(url):
     # it into OpenCV format
     request = urllib.request.Request(url, headers=headers)
     resp = urllib.request.urlopen(request).read()
-    image = np.asarray(bytearray(resp), dtype="uint8")
+    image = numpy.asarray(bytearray(resp), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
  
     # return the image
@@ -28,35 +27,94 @@ def image_crop_square(img, new_size):
     crop_img = img[y : new_size + y, x : new_size + x]
     return crop_img
 
-def concatenate_images(cropped_images):
-    start_x = [0] + [0 for _ in cropped_images]
+def proper_round(num, dec=0):
+    num = str(num)[:str(num).index('.')+dec+2]
+    if num[-1]>='5':
+        return int(float(num[:-2-(not dec)]+str(int(num[-2-(not dec)])+1)))
+    return int(float(num[:-1])) 
+
+def size_same(images, height_not_width, crop_not_scale=False):
+    if height_not_width:
+        shape_dim = 0
+    else:
+        shape_dim = 1
+    min_size = min([image.shape[shape_dim] for image in images])
+    if crop_not_scale:
+        offsets = [int((image.shape[shape_dim] - min_size) / 2) for image in images] 
+        if height_not_width:
+            new_images = [image[offset : offset + min_size, :] for offset, image in zip(offsets, images)]
+        else: 
+            new_images = [image[:, offset : offset + min_size] for offset, image in zip(offsets, images)]
+    else:
+        scale_factors = [min_size/image.shape[shape_dim] for image in images]
+        new_images = [cv2.resize(image, dsize=None, fx=scale_factor, fy=scale_factor) for (scale_factor, image) in zip(scale_factors, images)] 
+    for image in new_images:
+        print(image.shape)       
+    return new_images 
+
+def create_image(height, width):
+    new_image = numpy.zeros((int(height), int(width), 3), numpy.uint8)
+    return new_image
+
+def save_image(filename, image):
+    #cv2.imshow('img-windows', image)
+    #cv2.waitKey(0)
+    filename_path = os.path.expanduser('~/Desktop/' + filename + '.jpg')
+    print(filename_path)
+    cv2.imwrite(filename_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), IMWRITE_JPEG_QUALITY])        
+
+def concatenate_images(images, horizontally_not_vertically, same_images_size, width_white_pc = .05):   
+    new_images = size_same(images,     height_not_width=horizontally_not_vertically, crop_not_scale=False)
+    if same_images_size:
+        new_images = size_same(new_images, height_not_width=(not horizontally_not_vertically), crop_not_scale=True)
+    if horizontally_not_vertically:
+        dim_same_size = 0
+    else:
+        dim_same_size = 1
+    same_size = new_images[0].shape[dim_same_size]
+    print(same_size)
+    qty_images = len(new_images)
+    sum_images_diff_sizes=0
+    for image in new_images:
+        sum_images_diff_sizes += image.shape[1 - dim_same_size]
+    if horizontally_not_vertically:   
+        width_white = int(sum_images_diff_sizes / (1. - width_white_pc * (qty_images - 1)) * width_white_pc)
+    else:
+        width_white = int(same_size * width_white_pc)
+    print(width_white)    
+    total_new_size = width_white * (qty_images - 1) + sum_images_diff_sizes
+    print(total_new_size)    
     
-    total_height, start_x[1], _ = cropped_images[0].shape
+    if horizontally_not_vertically:
+        big_image = create_image(same_size, total_new_size)
+    else:
+        big_image = create_image(total_new_size, same_size)        
+    big_image.fill(255)
+    print(big_image.shape)    
     
-    for i in range(1, len(cropped_images)):
-        height, width, _ = cropped_images[i].shape
-        start_x[i+1] = start_x[i] + width
-        if total_height > height:
-            total_height = height
-    
-    big_image = np.zeros((total_height, start_x[-1], 3), np.uint8)
-    
-    for i in range(len(cropped_images)):
-        big_image[:, start_x[i] : (start_x[i+1])] = cropped_images[i][0:total_height, :]
-        
+    counter=0
+    for image in new_images:
+        add_size = image.shape[1 - dim_same_size]
+        print(counter, (counter + add_size))
+        print(image.shape)
+        if horizontally_not_vertically:
+            big_image[:, counter : (counter + add_size)] = image
+        else:
+            big_image[counter : (counter + add_size), :] = image
+        counter += add_size + width_white
+    cv2.imshow('img-windows', big_image)    
+    cv2.waitKey(0)
     return big_image
 
 def crop_concatenate_resize(urls, filename, cropped_size, final_width):
     cropped_images = [image_crop_square(img = url_to_image(url), new_size = cropped_size) for url in urls]      
-    big_image = concatenate_images(cropped_images)  
+    big_image = concatenate_images(cropped_images, horizontally_not_vertically = True, same_images_size = True, width_white_pc = 0)  
     
     _, width, _ = big_image.shape          
     resize_factor = final_width / width
     small = cv2.resize(big_image, (0,0), fx = resize_factor, fy = resize_factor)
-         
-    cv2.imwrite(os.path.expanduser('~/Desktop/' + filename + '.jpg'), small, [int(cv2.IMWRITE_JPEG_QUALITY), IMWRITE_JPEG_QUALITY])        
-        # cv2.imshow('img-windows',small)
-        # cv2.waitKey(0)
+    
+    save_image(filename, small)       
 
 if __name__ == '__main__':
     mfas = [# [950, "http://mfas3.s3.amazonaws.com/objects/SC246818.jpg", "http://mfas3.s3.amazonaws.com/objects/SC246819.jpg", "stater-of-taras-tarentum-with-horse-and-rider-struck-under-philokles-955"], 
@@ -78,13 +136,18 @@ if __name__ == '__main__':
             # [850, "http://mfas3.s3.amazonaws.com/objects/SC251695.jpg", "http://mfas3.s3.amazonaws.com/objects/SC251696.jpg", "trihemidrachm-of-corinth-with-bellerophon-riding-pegasos-1451"], 
             # [850, "http://mfas3.s3.amazonaws.com/objects/SC269208.jpg", "http://mfas3.s3.amazonaws.com/objects/SC269209.jpg", "coin-of-thyatira-with-bust-of-severus-alexander-struck-under-mar-pollianus-264438"], 
             # [1200, "http://mfas3.s3.amazonaws.com/objects/SC266375.jpg", "http://mfas3.s3.amazonaws.com/objects/SC266376.jpg", "coin-of-corinth-with-head-of-aphrodite-struck-under-q-caecilius-niger-and-c-heius-pamphilus-259879"], 
-            [800, "http://mfas3.s3.amazonaws.com/objects/SC251274.jpg", "http://mfas3.s3.amazonaws.com/objects/SC251275.jpg", "drachm-of-kibyra-with-head-of-youth-3507"], 
+            # [800, "http://mfas3.s3.amazonaws.com/objects/SC251274.jpg", "http://mfas3.s3.amazonaws.com/objects/SC251275.jpg", "drachm-of-kibyra-with-head-of-youth-3507"], 
             # [650, "", "", "ikmk-"], 
             # [950, "", "", ""], 
             
             ]
 
-    for mfa in mfas:
-        crop_concatenate_resize(urls = mfa[1:-1], filename = mfa[-1], cropped_size = mfa[0], final_width = 1024)
+    # for mfa in mfas:
+    #    crop_concatenate_resize(urls = mfa[1:-1], filename = mfa[-1], cropped_size = mfa[0], final_width = 1024)
+    
+    images = [url_to_image('http://www.yu51a5.com/wp-content/uploads/horsemen/' + fn) for fn in ['AN00149889_001_l.jpg',
+                                                                                   'AN00150545_001_l.jpg',
+                                                                                   'AN00150544_001_l.jpg']]
+    concatenate_images(images, horizontally_not_vertically = False, same_images_size = True, width_white_pc = .05)
  
 
