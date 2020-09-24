f5c54431ffd832f36ecd460fe6a78ed74541fa80
Yulia V <yvoev@mail.ru>
2020-09-24 17:26:05 +0300

    exposed tolerance, solution_units bug, add str solution

--- Tutoring/tutorship_reusable.py
+++ Tutoring/tutorship_reusable.py
@@ -26,9 +26,17 @@ def check_quiz_solution(what_are_we_looking_for, correct_solution, solution_unit
         solution_units_to_use = ""
     user_solution_string = input(question + "?")
 
-    try:                                                             # if the solution is a number
-        user_solution_number = float(user_solution_string)
-        if abs(user_solution_number - correct_solution) < tolerance: # if it is very close to the correct number
+    try:                                                             # if the solution is a the right format
+        correct = False
+        if isinstance(correct_solution, str):
+            if correct_solution == user_solution_number:
+                correct = True
+        else:
+            user_solution_number = float(user_solution_string)
+            if abs(user_solution_number - correct_solution) < tolerance: # if it is very close to the correct number
+                correct = True
+
+        if correct:
             conclusion = "This is correct, congratulations!"         # then congratulations
         else:
             conclusion = "Unfortunately this is incorrect."          # otherwise better luck next time
@@ -46,8 +54,9 @@ def check_quiz_solution(what_are_we_looking_for, correct_solution, solution_unit
 #            it returns None if we don't save it             
 def plt_envelope(func_to_call, save_file = True, add_grid = False, 
                  figsize = (5, 5), dpi = 300, x_tick_step = None, y_tick_step = None,
-                 solution_object = None, title_addon = None,
+                 solution_object = None, title_addon = None, tolerance = 0.0001,
                  **kwargs):
+            
     fig = plt.figure(figsize = figsize, dpi = dpi) # create the figure
     ax = fig.add_subplot(111)
 
@@ -127,9 +136,14 @@ def plt_envelope(func_to_call, save_file = True, add_grid = False,
     if solution_object is not None:
         if not solution_object['show_solutions']:
             for solution in solution_object['solutions']:
+                if len(solution) > 2:
+                    solution_units = solution[2]
+                else:
+                    solution_units = None
                 quiz_result = check_quiz_solution(what_are_we_looking_for = solution[0], 
                                                   correct_solution = solution[1], 
-                                                  solution_units = solution[2])
+                                                  solution_units = solution_units,
+                                                  tolerance = tolerance)
                 print('\n'.join(quiz_result))
 
     return filename_with_path
