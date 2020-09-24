86c6e3935078c818a4f696f5c7adc69d7a5d3217
Yulia V <yvoev@mail.ru>
2020-09-24 13:47:10 +0300

    moved grid code around, work in progress on the ticks

--- Tutoring/tutorship_reusable.py
+++ Tutoring/tutorship_reusable.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.path as mpath
 import matplotlib.patches as mpatches
@@ -6,6 +6,16 @@ import matplotlib.lines as mlines
 import matplotlib.ticker as mticker
 from datetime import datetime
 import os
+
+plt.rcParams.update({
+    "font.size": 5,
+    "pgf.texsystem": "pdflatex",
+    "pgf.preamble": [
+         r"\usepackage[utf8x]{inputenc}",
+         r"\usepackage[T1]{fontenc}",
+         r"\usepackage{cmbright}",
+         ]
+})
 
 def check_quiz_solution(what_are_we_looking_for, correct_solution, solution_units = None, tolerance = 0.0001):
     question = "What is " + what_are_we_looking_for                  # put together the quesion
@@ -35,27 +45,55 @@ def check_quiz_solution(what_are_we_looking_for, correct_solution, solution_unit
 # this function returns the filename of the saved figure if we save it,
 #            it returns None if we don't save it             
 def plt_envelope(func_to_call, save_file = True, add_grid = False, 
-                 figsize = (5, 5), dpi = 300, 
-                 solution_object = None, 
+                 figsize = (5, 5), dpi = 300, x_tick_step = None, y_tick_step = None,
+                 solution_object = None, title_addon = None,
                  **kwargs):
-    plt.close()
-
     fig = plt.figure(figsize = figsize, dpi = dpi) # create the figure
     ax = fig.add_subplot(111)
 
-    for spine_id in ['left', 'bottom']:
-        ax.spines[spine_id].set_position('zero')
-        ax.spines[spine_id].set_smart_bounds(True)
-    for spine_id in ['right', 'top']:
-        ax.spines[spine_id].set_color('none')
-
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')                     
-
     func_to_call(ax = ax, **kwargs)    
 
-    if add_grid:
-        ax.grid(True, which='both')                # add a grid   
+    if add_grid: 
+        # do spines
+        for spine_id in ['left', 'bottom']:
+            ax.spines[spine_id].set_position('zero')
+            ax.spines[spine_id].set_smart_bounds(True)
+        for spine_id in ['right', 'top']:
+            ax.spines[spine_id].set_color('none')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')                     
+
+        # make sure 0s are included in 0X
+        left_x, right_x = ax.get_xlim()  # return the current xlim
+        if left_x > 0:
+            left_x -= np.ceil(left_x)
+            ax.set_xlim(left = left_x)
+        if right_x < 0:
+            right_x -= np.floor(left_x)
+            ax.set_xlim(right = right_x)
+        ax.spines['bottom'].set_bounds(left_x, right_x)
+        ax.set_xlim(left_x, right_x)
+        x_ticks = []
+        ax.xticks(x_ticks)
+        loc_x = mticker.MultipleLocator(base=x_tick_step) # this locator puts ticks at regular intervals
+        ax.xaxis.set_major_locator(loc_x)
+
+        # make sure 1s are included in 0Y
+        bottom_y, top_y = ax.get_ylim()  # return the current ylim
+        if bottom_y > 0:
+            bottom_y -= np.ceil(bottom_y)
+            ax.set_ylim(bottom = bottom_y)
+        if top_y < 0:
+            top_y -= np.floor(top_y)
+            ax.set_ylim(top = top_y)
+        ax.spines['left'].set_bounds(bottom_y, top_y)
+        ax.set_ylim(bottom_y, top_y)
+        y_ticks = []
+        ax.yticks(y_ticks)
+        loc_y = mticker.MultipleLocator(base=y_tick_step) # this locator puts ticks at regular intervals
+        ax.yaxis.set_major_locator(loc_y)
+
+        ax.grid(add_grid, which='both', xdata=x_ticks, ydata=y_ticks)  # add a grid   
 
     if solution_object is not None:
         if solution_object['show_solutions']:
@@ -65,9 +103,13 @@ def plt_envelope(func_to_call, save_file = True, add_grid = False,
                 if len(solution) > 2:
                     solution_sentence += solution[2]
                 label_friendly_solutions.append(solution_sentence)
+            label_friendly_solutions.append("")
             plt.title('\n'.join(label_friendly_solutions)) 
 
-    plt.tight_layout()          
+    if title_addon is not None:
+        plt.suptitle(title_addon)
+
+    plt.tight_layout()         
 
     if save_file:
         # generate timestamp for the filename to make it unique
@@ -76,7 +118,9 @@ def plt_envelope(func_to_call, save_file = True, add_grid = False,
             now = now.replace(c, '_')
         # put together a filename and its path
         filename_with_path = os.path.expanduser('~/Documents/Python/Tutoring/Images/matlibplot' + now + '.png')
-        plt.savefig(filename_with_path)    
+        plt.savefig(filename_with_path)  
+    else:
+        filename_with_path = None  
 
     plt.show() # show! 
 
@@ -91,7 +135,7 @@ def plt_envelope(func_to_call, save_file = True, add_grid = False,
     return filename_with_path
 
 def plot_point(ax, x, y, colour = '', marker = '', annotiation = '', 
-               show_coords = True, do_projections = True, do_lines = False,
+               show_coords = True, do_projections = False, do_lines = False,
                tolerance = 0.0001):
     if (abs(x) > tolerance) and (abs(y) > tolerance) and do_projections:
         if do_lines:
@@ -112,8 +156,7 @@ def plot_grid(ax, x, y):
     ax.xaxis.set_major_locator(loc)
     ax.yaxis.set_major_locator(loc)
 
-# plt_envelope(plot_point, x = 1, y = 2, annotiation = 'A', colour = 'r', marker = 'o', do_lines = True)
-# plt_envelope(plot_grid, x = 10, y = 10, add_grid = True)
+#plt_envelope(plot_grid, x = 10, y = 10, add_grid = True)
 
 def create_vertical_arrow(arrow_x, 
                           arrowhead_width, # not used
@@ -126,7 +169,7 @@ def create_vertical_arrow(arrow_x,
         colour_shaft = colour_arrowhead # use the same colour as the colour of the arrowhead!
 
                                                             # define a symmetric 1-by-1 arrowhead
-    arrowhead_outline = numpy.array([[-0.5, 1.0], [0.5, 1.0], [0.0, 0.0], [-0.5, 1.0]])   
+    arrowhead_outline = np.array([[-0.5, 1.0], [0.5, 1.0], [0.0, 0.0], [-0.5, 1.0]])   
     arrowhead_outline *= arrowhead_tip_height               # scale it
     if (end_shaft < arrowhead_tip_y):                       # if the end of the tip is above the end of the shaft,
         arrowhead_outline[:, 1] *= -1                       # flip it upside down
